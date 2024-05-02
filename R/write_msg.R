f71ca7a6a18fb4dfb0afea67ba7b42135a6142d0
Mathias Lerbech Jeppesen <NMLJ@novonordisk.com>
2024-05-02 17:07:46 +0200

    feat: Added write_msg function

---
+++ R/write_msg.R
@@ -0,0 +1,46 @@
+#' Write messages based on verbosity level
+#'
+#' @description
+#'
+#' Write based on verbosity level set as option in package
+#'
+#' @param message `character` of message to write
+#' @param x Code to be printed based on verbosity level
+#' @param verbosity_levels `character` vector of levels of verbosity for which
+#' to display the message
+#' @param msg_fun `function` taking `message` as first argument. Usually a
+#' `cli_...` function
+#' @param ... Additional arguments passed to `msg_fun`
+#' @param verbosity_levels `character` vector of levels of verbosity for which
+#'
+#' @examples
+#' # Use the `write_msg` function to fx. create headlines for big steps in the
+#' # TFL creation process
+#' write_msg("Initialising {.field ingest_tfl} object",
+#'           verbosity_levels = c("verbose", "debug"),
+#'           msg_fun = cli::cli_h1)
+#'
+#' ingest_init(pharmaverseadam::adae, filter = SAFFL == "Y")
+#'
+#' # Use the `with_verbose` function to
+#' data %>%
+#'   dplyr::filter(SAFFL == "Y") %>%
+#'   with_verbose()
+#'
+#' @export
+write_msg <- function(message,
+                      verbosity_levels = c("verbose", "debug"),
+                      msg_fun = cli::cli_h1,
+                      ...) { # TODO add a destination argument to control
+  # whether to write to console, write to a file, etc.
+
+  match.arg(verbosity_levels,
+            choices = c("quiet", "verbose", "debug"),
+            several.ok = TRUE)
+
+  if (options::opt("verbosity_level") %in% verbosity_levels) {
+    msg_fun(message, ...)
+  }
+
+  invisible()
+}
