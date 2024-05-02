c87502a167f1fe963a7ee9eddce5370504875432
Mathias Lerbech Jeppesen <NMLJ@novonordisk.com>
2024-05-02 18:03:33 +0200

    naming: Changed name of argument

--- R/write_msg.R
+++ R/write_msg.R
@@ -6,18 +6,18 @@
 #'
 #' @param message `character` of message to write
 #' @param x Code to be printed based on verbosity level
-#' @param write_for_levels `character` vector of levels of verbosity for which
+#' @param levels_to_write `character` vector of levels of verbosity for which
 #' to display the message
 #' @param msg_fun `function` taking `message` as first argument. Usually a
 #' `cli_...` function
 #' @param ... Additional arguments passed to `msg_fun`
-#' @param write_for_levels `character` vector of levels of verbosity for which
+#' @param levels_to_write `character` vector of levels of verbosity for which
 #'
 #' @examples
 #' # Use the `write_msg` function to fx. create headlines for big steps in the
 #' # TFL creation process
 #' write_msg("Initialising {.field ingest_tfl} object",
-#'           write_for_levels = c("verbose", "debug"),
+#'           levels_to_write = c("verbose", "debug"),
 #'           msg_fun = cli::cli_h1)
 #'
 #' ingest_init(pharmaverseadam::adae, filter = SAFFL == "Y")
@@ -29,18 +29,18 @@
 #'
 #' @export
 write_msg <- function(message,
-                      write_for_levels = c("verbose", "debug"),
+                      levels_to_write = c("verbose", "debug"),
                       msg_fun = cli::cli_h1,
                       ...) { # TODO add a destination argument to control
   # whether to write to console, write to a file, etc.
 
-  match.arg(write_for_levels,
+  match.arg(levels_to_write,
             choices = c("quiet", "verbose", "debug"),
             several.ok = TRUE)
 
   verbosity_level <- get_option("verbosity_level")
 
-  if (verbosity_level %in% write_for_levels) {
+  if (verbosity_level %in% levels_to_write) {
     msg_fun(message, ...)
   }
 
