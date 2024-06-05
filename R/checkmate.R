db7e90d009fe8e428a7b585f70345d4b6462f9d1
Aksel Thomsen (OATH) <oath@novonordisk.com>
2024-06-05 09:40:59 +0200

    fi: linting

--- R/checkmate.R
+++ R/checkmate.R
@@ -1,9 +1,11 @@
 #' Report collection of assertations
 #' @description
 #' Improved reporting of `AssertColellections` created with the [checkmate::makeAssertCollection()] package
-#' using [cli::cli_abort()] instead of [checkmate::reportAssertions()] in order to provide a more informative error message.
+#' using [cli::cli_abort()] instead of [checkmate::reportAssertions()] in order to provide a more
+#' informative error message.
 #'
-#' THe function is inteded to be used inside a function that performs assertations on its input arguments. See below for an example.
+#' THe function is intended to be used inside a function that performs assertations on its input arguments.
+#' See below for an example.
 #' @param collection A collection of assertations created with [checkmate::makeAssertCollection()]
 #' @param msg [character()] Header of the error message if any assertations failed
 #' @param env [environment()] Environment to use for the error message
@@ -23,14 +25,11 @@
 #' @export
 
 report_checkmate_assertations <- function(collection, msg = "Invalid input(s):", env = parent.frame()) {
-
   checkmate::assert_class(collection, "AssertCollection")
 
-  # env <- parent.frame()
   if (!collection$isEmpty()) {
     c(msg, rlang::set_names(collection$getMessages(), "x")) |>
       cli::cli_abort(.envir = env)
   }
   invisible(TRUE)
 }
-
