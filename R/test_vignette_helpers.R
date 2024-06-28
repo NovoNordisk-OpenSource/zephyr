c26e9fb228efe67decc626cab97e39cda8900b00
NMLJ (Mathias Lerbech Jeppesen) <nmlj@novonordisk.com>
2024-06-28 09:58:38 +0200

    ci: Avoided CRAN note by using utils::getFromNamespace instead of ::: to get unexported function

--- R/test_vignette_helpers.R
+++ R/test_vignette_helpers.R
@@ -1,5 +1,6 @@
 create_env_with_fun <- function(fun_name = "foo",
                                 message = "test",
+                                add_option = TRUE,
                                 default = "verbose",
                                 fun = function(a = 1, b = 2, ...) {
                                   msg(message, ...)
@@ -9,14 +10,17 @@ create_env_with_fun <- function(fun_name = "foo",
   environment(fun) <- e
   assign(fun_name, fun, envir = e)
 
-  # Using option spec followed by unexported set_option_spec instead of using
-  # define_option due to a bit weird behavior with environments during testing
-  option <- options::option_spec("verbosity_level",
-                                 default,
-                                 desc = "tst",
-                                 option_name = paste0(fun_name, "_pkg.verbosity_level"),
-                                 envvar_name = paste0("R_", toupper(fun_name), "_PKG_VERBOSITY_LEVEL"))
-  options:::set_option_spec("verbosity_level", option, env = e)
+  if (add_option) {
+    # Using option spec followed by unexported set_option_spec instead of using
+    # define_option due to a bit weird behavior with environments during testing
+    option <- options::option_spec("verbosity_level",
+                                   default,
+                                   desc = "Option for testing",
+                                   option_name = paste0(fun_name, "_pkg.verbosity_level"),
+                                   envvar_name = paste0("R_", toupper(fun_name), "_PKG_VERBOSITY_LEVEL"))
+    set_option_spec <- utils::getFromNamespace("set_option_spec", "options")
+    set_option_spec("verbosity_level", option, env = e)
+  }
 
   return(e)
 }
