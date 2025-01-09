a044ba73bb7dbeb0bb6bf01a096d7d5d7e30aa4e
akselthomsen <thomsenaksel@gmail.com>
2025-01-09 09:34:40 +0100

    fix: don't snapshot the cli id of the messages

--- tests/testthat/_snaps/integration.md
+++ tests/testthat/_snaps/integration.md
@@ -1,14 +1,17 @@
 # integration in new package
 
     Code
-      run_output_project(function() zephyr::use_zephyr(), libpath, testpkg)
+      run_output_project(function() {
+        zephyr::use_zephyr()
+        NULL
+      }, libpath, testpkg)
     Output
       
       ── Setting up zephyr ───────────────────────────────────────────────────────────
       ℹ Add new options with `zephyr::create_option()`.
       ℹ And reuse their documentation with in functions with `@inheritParams testpkg-options-params`.
       ℹ Run `devtools::document()` to update documentation.
-      [1] "cli-85346-14"
+      NULL
 
 ---
 
--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -47,7 +47,7 @@ test_that("integration in new package", {
 
   # Use in new package
 
-  run_output_project(\() zephyr::use_zephyr(), libpath, testpkg) |>
+  run_output_project(\() {zephyr::use_zephyr(); NULL}, libpath, testpkg) |>
     expect_snapshot()
 
   # Only to not get warnings below
@@ -55,13 +55,6 @@ test_that("integration in new package", {
 
   run_output_project(\() devtools::document(quiet = TRUE), libpath, testpkg) |>
     expect_snapshot()
-
-  # run_output_project(
-  #   \() rcmdcheck::rcmdcheck(quiet = TRUE, error_on = "warning"),
-  #   libpath,
-  #   testpkg
-  # ) |>
-  #   expect_no_error()
 
   run_output_project(
     \() devtools::install(dependencies = FALSE, quiet = TRUE),
